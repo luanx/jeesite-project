fe73f3ca527f7fef0ad372c6446bc7fc3a3c9315
luanx <luanx@wantdo.com>
2019-03-07 15:36:34 +0800

    add basic config

---
+++ jeesite-web/src/main/java/com/wantdo/web/Application.java
@@ -0,0 +1,16 @@
+package com.wantdo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luanx
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
